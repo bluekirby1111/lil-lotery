18f734e4c5e0e86492d7b5edafa5c3d80ab551b6
viknupp <arq.vitoriaknupp@gmail.com>
2023-07-27 22:19:06 +0100

    feat: header styling

--- app/header.tsx
+++ app/header.tsx
@@ -4,16 +4,19 @@ interface HeaderProperties {
   displayText: string;
 }
 
-const navigationItems = ["FAQ", "About us", "Contact"].map((key) => ({
-  key,
-  label: key,
-}));
+const navigationItems = ["FAQ", "About us", "Contact"].map((key) => (
+  <li className={styles.navItems}>
+    <a href="">{key}</a>
+  </li>
+));
 
 const Header: React.FC<HeaderProperties> = ({ displayText }) => {
   return (
     <div className={styles.header}>
-      <h1>lightning</h1>
-      <nav></nav>
+      <h1 className={styles.title}>lightning</h1>
+      <nav>
+        <ul className={styles.navList}>{navigationItems}</ul>
+      </nav>
     </div>
   );
 };
